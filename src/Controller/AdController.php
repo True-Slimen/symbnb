543cb583191b82927e1fbd2ffe29da910a1fc38a
True-Slimen <metatidj.slimen@gmail.com>
2020-04-24 10:38:25 +0200

    synchro

---
+++ src/Controller/AdController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+         // $repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads 
+        ]);
+    }
+
+     /**
+     * Permet de creer une annonce
+     * 
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response 
+     */
+    public function create(){
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(Ad $ad){
+
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+
+   
+}
