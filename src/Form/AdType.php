91e6adef63367181b97d56e647249bb9036b3a9f
Slimen <metatidj.slimen@gmail.com>
2020-04-29 12:54:43 +0200

    création et intégration du formulaire pour la création d'annonce

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -6,20 +6,43 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AdType extends AbstractType
 {
+
+    /**
+     * Permet d'avoir la configuration de base d'un champ.
+     *
+     * @param [string] $label
+     * @param [string] $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder){
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+            ];
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
+            ->add('title', TextType::class, $this->getConfiguration("Titre", "Définissez un titre à votre annonce"))
+            ->add('slug', TextType::class, $this->getConfiguration("Adresse web", "Choix de l'adresse url (facultatif)"))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image principale", "Inserez l'url de votre plus belle image"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Creez un introduction courte"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description", "Creez la description de l'annonce") )
+            ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit", "Définissez le prix par nuit"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Le nombres de chambres disponibles"))
             // ->add('save', SubmitType::class)
         ;
     }
